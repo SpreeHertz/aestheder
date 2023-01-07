38c0e9f5f904b968f1a07ee61320f376082ea68f
SpreeHertz <spreehertzofficial@gmail.com>
2023-01-07 19:23:55 +0530

    feat: new handler

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const { Client, Collection } = require("discord.js");
-require('dotenv').config();
-const client = new Client({
-    intents: 32767,
+// Require the necessary discord.js classes
+require("dotenv").config();
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const interactionCreate = require("./src/events/client/interactionCreate");
+
+client.on("messageReactionAdd", (reaction, user) => {
+  if (user.bot) return;
+  if (reaction.partial) {
+    try {
+      reaction.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message: ", error);
+      return;
+    }
+  }
+  interactionCreate.execute(reaction, client);
 });
-module.exports = client;
 
-// Global Variables
 client.commands = new Collection();
-client.slashCommands = new Collection();
-client.config = require("./config.json");
+client.commandArray = [];
 
-// Initializing the project
-require("./handler")(client);
+const functionFolders = fs.readdirSync("./src/functions");
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
 
-client.login(client.config.token);
+  for (const file of functionFiles) {
+    require(`./src/functions/${folder}/${file}`)(client);
+  }
+}
+
+client.handleEvents();
+client.handleCommands();
+// Log in to Discord with your client's token
+// put token in .env file
+client.login(process.env.token);
---
+++ src/commands/info/ping.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription(
+      "🏓 Ping the bot"
+    ),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+      fetchReply: true,
+    });
+    const newMessage = `API Latency: ${client.ws.ping}ms\nClient Ping: ${
+      message.createdTimestamp - interaction.createdTimestamp
+    }ms`;
+    await interaction.editReply({
+      content: newMessage,
+    });
+
+    return interaction.reply("Pong!");
+  },
+};
